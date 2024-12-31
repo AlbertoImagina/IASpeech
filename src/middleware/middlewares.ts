da8ec8328d2f358931520a44d56807ba85047730
carlosalberto <carlosalberto@imaginagroup.com>
2024-12-31 09:56:19 +0100

    Ocultamos url de la api

--- src/middleware/middlewares.ts
+++ src/middleware/middlewares.ts
@@ -2,6 +2,10 @@ import axios from "axios"
 
 const token = import.meta.env.VITE_TOKEN_APP;
 
+const apiUrl = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+})
+
 export const enviarVoz = async ({ pregunta, guiaCorreccion, archivo, dificultad }: { pregunta: string; guiaCorreccion: string; archivo: File; dificultad: number }) => {
   const formData = new FormData()
   formData.append("pregunta", pregunta)
@@ -10,8 +14,8 @@ export const enviarVoz = async ({ pregunta, guiaCorreccion, archivo, dificultad 
   formData.append("dificultad", dificultad.toString())
 
   try {
-    const { data } = await axios.post(
-      "https://api-beta.proacademy.app/v1/imaginabeta/admin/ai/corrigePreguntaAudio",
+    const { data } = await apiUrl.post(
+      "/corrigePreguntaAudio",
       formData,
       {
         headers: {
